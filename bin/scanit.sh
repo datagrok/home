b2171b93f7ab278ad2afa5e3b469e8c0934edda1
Michael F. Lamb <mike@datagrok.org>
2013-12-04 19:47:37 -0500

    scan destination directory hueristic

--- bin/scanit.sh
+++ bin/scanit.sh
@@ -2,7 +2,14 @@
 set -e
 
 if [ ! "$1" ]; then
-    exec scanadf --source "ADF Duplex" --threshold 128 -d fujitsu -S "$0"
+    exec scanadf --source "ADF Front" --threshold 128 -d fujitsu -S "$0"
+    #exec scanadf --source "ADF Front" --threshold 128 -d fujitsu --page-height 355.6 -y 355.6 -S "$0"
+    #exec scanadf --source "ADF Duplex" --threshold 128 -d fujitsu -S "$0"
+fi
+
+output_dir=~/net/doc/Scanned
+if [ ! -d "$output_dir" ]; then
+    output_dir=~/doc/scanned
 fi
 
 # $1 - the image filename
@@ -13,10 +20,10 @@ fi
 # $SCAN_FORMAT - a string representing the image format (e.g. gray, g42d, text, etc)
 # $SCAN_FORMAT_ID - the numeric image format identifier
 
-mkdir -p ~/doc/scanned
+mkdir -p "${output_dir}"
 unpaper --overwrite --pre-border 50,0,50,100 -dn left,top,right,bottom "$1" "up-$1"
 rm "$1"
-filename=~/doc/scanned/"$(date +%s)-$1.djvu"
+filename="${output_dir}/$(date +%s)-$1.djvu"
 cjb2 -dpi "$SCAN_RES" -clean -lossy "up-$1" "$filename"
 rm "up-$1"
 echo "Stored: $filename"
